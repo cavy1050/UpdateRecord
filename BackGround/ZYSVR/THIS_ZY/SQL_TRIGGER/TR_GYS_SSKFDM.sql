edc811dedf017197659241ba94cdf187448143a5
cavy1050 <cavy1050@163.com>
2020-03-30 09:15:46 +0800

    link #28 供应室-消毒包无法进行回收登记

---
+++ BackGround/ZYSVR/THIS_ZY/SQL_TRIGGER/TR_GYS_SSKFDM.sql
@@ -0,0 +1,23 @@
+IF OBJECT_ID('TR_GYS_SSKFDM','TR') IS NOT NULL
+DROP TRIGGER TR_GYS_SSKFDM
+GO
+
+CREATE TRIGGER TR_GYS_SSKFDM ON dbo.GYS_XDBSMQK AFTER INSERT
+AS
+
+/*****************************
+--add by yangdi 2020.3.29
+增加触发器处理消毒包首次打包后，GYS_XDBSMQK.sskfdm,GYS_XDBSMQK.sskfmc为null,导致不能进行回收问题。
+GYS_XDBSMQK.sskfdm=GYS_XDBSMQK.kfdm
+GYS_XDBSMQK.sskfmc=GYS_XDBSMQK.kfmc
+*****************************/
+
+SET NOCOUNT ON
+
+IF EXISTS(SELECT 1 FROM INSERTED) AND NOT EXISTS (SELECT 1 FROM DELETED)
+BEGIN
+	UPDATE b SET b.sskfdm=a.kfdm,b.sskfmc=a.kfmc FROM INSERTED a
+		INNER JOIN dbo.GYS_XDBSMQK b ON a.xh=b.xh
+END
+
+RETURN
